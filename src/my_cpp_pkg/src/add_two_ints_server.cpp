d3b0654cf83f3d4bff7af045d1af9528625b8bfc
Lee Dongwon Ubuntu <dlehddnjs245@khu.ac.kr>
2025-07-29 12:16:12 +0900

    cpp service node

---
+++ src/my_cpp_pkg/src/add_two_ints_server.cpp
@@ -0,0 +1,33 @@
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/srv/add_two_ints.hpp"
+using namespace std::placeholders;
+
+class AddTwoIntsServerNode : public rclcpp::Node
+{
+public:
+        AddTwoIntsServerNode(): Node("add_two_ints_server")
+        {
+            server_ = this -> create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", std::bind(&AddTwoIntsServerNode::callbackAddTwoInts, this, _1, _2));
+            RCLCPP_INFO(this->get_logger(), "Add Two Ints Server has been started.");
+        }
+private:
+
+
+    void callbackAddTwoInts(
+        const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
+        std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response)
+    {
+        response->sum = request->a + request->b;
+        RCLCPP_INFO(this->get_logger(), "%ld + %ld = %ld", request->a, request->b, response->sum);
+    }
+
+    rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr server_;
+};
+
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<AddTwoIntsServerNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
